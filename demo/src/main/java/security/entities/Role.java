8d2e460f28ce51e20801fd54238911e8e7598f22
junior99800 <Juniorcarros36@hotmail.com>
2026-09-02 17:14:38 -0300

    Join column das entities ROLE e USER

---
+++ demo/src/main/java/security/entities/Role.java
@@ -0,0 +1,39 @@
+package security.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String authority;
+
+    public Role(){
+    }
+
+    public Role(String authority, long id) {
+        this.authority = authority;
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(authority);
+    }
+}
--- demo/src/main/java/security/entities/User.java
+++ demo/src/main/java/security/entities/User.java
@@ -1,15 +1,17 @@
 package security.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_user")
 public class User {
 
     @Id
@@ -21,6 +23,12 @@ public class User {
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     public User(long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
@@ -29,6 +37,19 @@ public class User {
     }
 
     public User() {
+    }
+
+    public void addrole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName){
+        for(Role role : roles){
+            if (role.getAuthority().equals(roleName)){
+                return true;
+            }
+        }
+        return false;
     }
 
 
